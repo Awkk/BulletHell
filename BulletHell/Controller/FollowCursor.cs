2f55f25797752681ce62532141469c505471771d
Andrew Wong <kkwong125@gmail.com>
2021-01-23 00:12:17 -0800

    Changed FollowCursor and keep the GameObject always stay in screen when the cursor is out of screen

--- BulletHell/Controller/FollowCursor.cs
+++ BulletHell/Controller/FollowCursor.cs
@@ -11,7 +11,9 @@ namespace BulletHell.Controller {
     class FollowCursor : IController {
         public void UpdateLocation(GameObject obj) {
             Point cursor = GameArea.MainForm.PointToClient(Control.MousePosition);
-            obj.Location = new Point(cursor.X - obj.Width / 2, cursor.Y - obj.Height / 2);
+            int newX = (cursor.X - obj.Width / 2 < 0) || (cursor.X + obj.Width > GameArea.MainForm.Width) ? obj.Location.X : cursor.X - obj.Width / 2;
+            int newY = (cursor.Y - obj.Height / 2 < 0) || (cursor.Y + obj.Height*1.75 > GameArea.MainForm.Height) ? obj.Location.Y : cursor.Y - obj.Height / 2;
+            obj.Location = new Point(newX, newY);
         }
     }
 }
