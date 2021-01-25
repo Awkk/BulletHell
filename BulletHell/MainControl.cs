8d6fe08cb7b525bcddbc26900322d2c4b4de8134
Andrew Wong <kkwong125@gmail.com>
2021-01-25 00:17:17 -0800

    Changed absolute path of buttonsound to relative path

--- BulletHell/MainControl.cs
+++ BulletHell/MainControl.cs
@@ -7,96 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Media;
 
-namespace BulletHell
-{
-    public partial class MainControl : UserControl
-    {
-        public MainControl()
-        {
+namespace BulletHell {
+    public partial class MainControl : UserControl {
+
+        private SoundPlayer buttonSound;
+        public MainControl() {
             InitializeComponent();
+            buttonSound = new SoundPlayer(GetResoucePath("buttonSound.wav"));
+        }
+
+        private string GetResoucePath(string file) {
+            string currentPath = Path.GetDirectoryName(Application.ExecutablePath);
+            return Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(currentPath)), "Resources", file);
         }
 
 
         //Menu pictureBox mouse hover/leave effect
-        private void add_option_MouseHover(object sender, EventArgs e)
-        {
+        private void add_option_MouseHover(object sender, EventArgs e) {
             add_option.Image = Properties.Resources.AddServerHighlight;
 
-            //System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
-            //System.IO.Stream s = a.GetManifestResourceStream("<AssemblyName>.chimes.wav");
-            //SoundPlayer player = new SoundPlayer(s);
-            //player.Play();
-
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\kimer\Source\Repos\BulletHell\BulletHell\Resources\buttonSound.wav");
-            sound.Play();
+            buttonSound.Play();
         }
 
-        private void join_option_MouseHover(object sender, EventArgs e)
-        {
+        private void join_option_MouseHover(object sender, EventArgs e) {
             join_option.Image = Properties.Resources.JoinServerHighlight;
 
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\kimer\Source\Repos\BulletHell\BulletHell\Resources\buttonSound.wav");
-            sound.Play();
+            buttonSound.Play();
         }
 
-        private void options_option_MouseHover(object sender, EventArgs e)
-        {
+        private void options_option_MouseHover(object sender, EventArgs e) {
             options_option.Image = Properties.Resources.OptionsHighlight;
 
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\kimer\Source\Repos\BulletHell\BulletHell\Resources\buttonSound.wav");
-            sound.Play();
+            buttonSound.Play();
         }
 
 
-        private void quit_option_MouseHover(object sender, EventArgs e)
-        {
+        private void quit_option_MouseHover(object sender, EventArgs e) {
             quit_option.Image = Properties.Resources.QuitHighlight;
 
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\kimer\Source\Repos\BulletHell\BulletHell\Resources\buttonSound.wav");
-            sound.Play();
+            buttonSound.Play();
         }
 
-        private void add_option_MouseLeave(object sender, EventArgs e)
-        {
+        private void add_option_MouseLeave(object sender, EventArgs e) {
             add_option.Image = Properties.Resources.AddServer;
 
 
         }
 
-        private void join_option_MouseLeave(object sender, EventArgs e)
-        {
+        private void join_option_MouseLeave(object sender, EventArgs e) {
             join_option.Image = Properties.Resources.JoinServer;
 
 
         }
 
-        private void options_option_MouseLeave(object sender, EventArgs e)
-        {
+        private void options_option_MouseLeave(object sender, EventArgs e) {
             options_option.Image = Properties.Resources.OptionsOption;
 
 
 
         }
-        private void quit_option_MouseLeave(object sender, EventArgs e)
-        {
+        private void quit_option_MouseLeave(object sender, EventArgs e) {
             quit_option.Image = Properties.Resources.QuitOption;
 
 
         }
 
-        private void add_option_MouseClick(object sender, MouseEventArgs e)
-        {
-           
+        private void add_option_MouseClick(object sender, MouseEventArgs e) {
+
             GameArea game = new GameArea();
             game.Show();
         }
 
-        private void options_option_Click(object sender, EventArgs e)
-        {
+        private void options_option_Click(object sender, EventArgs e) {
 
-            if (!GameMenu.Instance.PnlContainer.Controls.ContainsKey("OptionControl"))
-            {
+            if (!GameMenu.Instance.PnlContainer.Controls.ContainsKey("OptionControl")) {
                 OptionControl oc = new OptionControl();
                 oc.Dock = DockStyle.Fill;
                 GameMenu.Instance.PnlContainer.Controls.Add(oc);
@@ -105,17 +93,15 @@ namespace BulletHell
         }
 
 
-        private void quit_option_Click(object sender, EventArgs e)
-        {
+        private void quit_option_Click(object sender, EventArgs e) {
             Application.Exit();
         }
 
-        private void add_option_Click(object sender, EventArgs e)
-        {
-         
+        private void add_option_Click(object sender, EventArgs e) {
+
         }
 
-        
+
     }
 
 
--- BulletHell/OptionControl.cs
+++ BulletHell/OptionControl.cs
@@ -3,64 +3,56 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BulletHell
-{
-    public partial class OptionControl : UserControl
-    {
-        public OptionControl()
-        {
+namespace BulletHell {
+    public partial class OptionControl : UserControl {
+        private SoundPlayer buttonSound;
+
+        public OptionControl() {
             InitializeComponent();
+            buttonSound = new SoundPlayer(GetResoucePath("buttonSound.wav"));
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
-        {
+        private string GetResoucePath(string file) {
+            string currentPath = Path.GetDirectoryName(Application.ExecutablePath);
+            return Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(currentPath)), "Resources", file);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e) {
             GameMenu.wplayer.controls.play();
             GameMenu.wplayer.settings.volume = trackBar1.Value;
 
-            if (trackBar1.Value == 0)
-            {
+            if (trackBar1.Value == 0) {
                 btn_sound.Image = Properties.Resources.sound_off;
-            }
-            else
-            {
+            } else {
                 btn_sound.Image = Properties.Resources.sound_on;
             }
         }
 
-        private void btn_sound_Click(object sender, EventArgs e)
-        {
-            if (trackBar1.Value > 0)
-            {
+        private void btn_sound_Click(object sender, EventArgs e) {
+            if (trackBar1.Value > 0) {
                 GameMenu.wplayer.settings.volume = 0;
                 btn_sound.Image = Properties.Resources.sound_off;
-
             }
-
         }
 
 
-            private void cancel_option_MouseHover(object sender, EventArgs e)
-        {
+        private void cancel_option_MouseHover(object sender, EventArgs e) {
             cancel_option.Image = Properties.Resources.CancelHighlight;
-
-            System.Media.SoundPlayer sound = new System.Media.SoundPlayer(@"C:\Users\kimer\Source\Repos\BulletHell\BulletHell\Resources\buttonSound.wav");
-            sound.Play();
+            buttonSound.Play();
         }
 
-        private void cancel_option_MouseLeave(object sender, EventArgs e)
-        {
+        private void cancel_option_MouseLeave(object sender, EventArgs e) {
             cancel_option.Image = Properties.Resources.Cancel;
-
-
         }
 
-        private void cancel_option_Click(object sender, EventArgs e)
-        {
+        private void cancel_option_Click(object sender, EventArgs e) {
             GameMenu.Instance.PnlContainer.Controls["MainControl"].BringToFront();
         }
     }
