0b264434a4a04f13b9fbfcd8cf6cf371c8a99f48
Andrew Wong <kkwong125@gmail.com>
2021-01-26 10:52:40 -0800

    DeathScreen modified

--- BulletHell/DeathScreenControl.cs
+++ BulletHell/DeathScreenControl.cs
@@ -10,74 +10,59 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
 
-namespace BulletHell
-{
+namespace BulletHell {
 
-    public partial class DeathScreenControl : UserControl
-    {
+    public partial class DeathScreenControl : UserControl {
         private SoundPlayer buttonSound;
 
-        private string GetResoucePath(string file)
-        {
+        private string GetResoucePath(string file) {
             string currentPath = Path.GetDirectoryName(Application.ExecutablePath);
             return Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(currentPath)), "Resources", file);
         }
-        public DeathScreenControl()
-        {
+        public DeathScreenControl() {
             InitializeComponent();
             blink_timer.Start();
             buttonSound = new SoundPlayer(GetResoucePath("buttonSound.wav"));
 
             GameArea parentForm = (this.Parent as GameArea);
-            
-           
+
+
         }
 
-        private void blink_Tick(object sender, EventArgs e)
-        {
+        private void blink_Tick(object sender, EventArgs e) {
             text_gameover.Visible = !text_gameover.Visible;
         }
 
-        private void text_mm_Click(object sender, EventArgs e)
-        {
-            ((Form)this.TopLevelControl).Close();
-            GameMenu menu = new GameMenu();
-            menu.Show();
+        private void text_mm_Click(object sender, EventArgs e) {
+            ((Form)TopLevelControl).Close();
         }
 
-        private void text_quit_Click(object sender, EventArgs e)
-        {
+        private void text_quit_Click(object sender, EventArgs e) {
             Application.Exit();
         }
 
-        private void visibility(object sender, EventArgs e)
-        {
+        private void visibility(object sender, EventArgs e) {
             this.Visible = true;
         }
 
-        private void text_mm_MouseHover(object sender, EventArgs e)
-        {
+        private void text_mm_MouseHover(object sender, EventArgs e) {
             buttonSound.Play();
         }
 
-        private void text_mm_MouseLeave(object sender, EventArgs e)
-        {
+        private void text_mm_MouseLeave(object sender, EventArgs e) {
 
         }
 
-        private void quit_text_MouseHover(object sender, EventArgs e)
-        {
+        private void quit_text_MouseHover(object sender, EventArgs e) {
 
             buttonSound.Play();
         }
 
-        private void quit_text_MouseLeave(object sender, EventArgs e)
-        {
+        private void quit_text_MouseLeave(object sender, EventArgs e) {
 
         }
 
-        public string showResults(string result)
-        {
+        public string showResults(string result) {
             result_lbl.Text = result;
             return result;
         }
